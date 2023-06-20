0150d11de1d5bf03d8fa34ecd47e4e099d83e5c1
W1nter-IsHere <w1nter.ish3re@gmail.com>
2023-06-20 15:10:11 +0000

    Huh

--- solutions/day3.py
+++ solutions/day3.py
@@ -6,3 +6,15 @@ sum = 0
 
 def priority_of(char) -> int:
     return ord(char) - 38 if char.isupper() else ord(char) - 96
+
+for index in range(len(lines)):
+    elf1 = set(lines[index])
+    elf2 = set(lines[index + 1])
+    elf3 = set(lines[index + 2])
+    index += 2
+
+    common12 = elf1.intersection(elf2)
+    common = common12.intersection(elf3)
+    sum += priority_of(common)
+
+print(sum)
